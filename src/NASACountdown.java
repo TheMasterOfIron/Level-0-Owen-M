656c99d8f688eaa9023d18f40e8d69888b3935b5
najclark <league@WTS7.attlocal.net>
2017-03-26 11:34:21 -0700

    day 6

---
+++ src/NASACountdown.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class NASACountdown {
+public static void main(String[] args) {
+	
+	String answer=JOptionPane.showInputDialog("What number do we start at?");
+	int a=Integer.parseInt(answer);
+	for (int c = a; c > 0; c--) {
+speak("t minus"+Integer.toString(c));
+	}
+speak("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiimmmmmmmmmmmmmmmmmmmmmmmmmmmmmm aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaannnnnnnnnnnnnnnnnnnnnnnnnnnnnnoooooooooooooooooooooooooooyyyyyyyyyyyyyyyyyyyyyyyyiiiiiiiiiiiiiiiiiiiiiiiiinnnnnnnnnnnnnnnnnnnnnnnngggggggggggggggggggggg");
+}
+static void speak(String words) {
+  	 try {
+  		 Runtime.getRuntime().exec("say " + words).waitFor();
+  	 } catch (Exception e) {
+  		 e.printStackTrace();
+  	 }
+ }
+
+}
---
+++ src/YearsAlive.java
@@ -0,0 +1,9 @@
+	public class YearsAlive {
+public static void main(String[] args) {
+	int b = 0;
+	for (int a = 2006; a < 2017; a++) {
+			System.out.print(" You were "+b+" in "+a+". ");
+			b++;
+		}
+	}
+}
