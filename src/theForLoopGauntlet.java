5ac9ed9326fcb7612f9f98c713390eef4943029e
amanddali <league@WTS19.attlocal.net>
2017-07-16 11:42:21 -0700

    rt5tgg
    iloliugfthujjgkfh

---
+++ src/theForLoopGauntlet.java
@@ -0,0 +1,11 @@
+import java.awt.print.Printable;
+
+public class theForLoopGauntlet {
+	public static void main(String[] args) {
+		for (int q = 1; q <= 7; q += 3) {
+			System.out.println(q + "" + (q + 1) + "" + (q + 2));
+		}
+
+	}
+
+}
