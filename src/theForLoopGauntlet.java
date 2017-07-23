68c5ea6d8424e8a254c0cee2abd857c69896ca9e
TheMasterOfIron <league@WTS19.attlocal.net>
2017-07-23 11:28:58 -0700

    yhyhtr5fg
    klhuygbvsxrtfkhnfvgbdfsafgvfrb y vj

--- src/theForLoopGauntlet.java
+++ src/theForLoopGauntlet.java
@@ -2,10 +2,8 @@ import java.awt.print.Printable;
 
 public class theForLoopGauntlet {
 	public static void main(String[] args) {
-		for (int q = 1; q <= 7; q += 3) {
-			System.out.println(q + "" + (q + 1) + "" + (q + 2));
+		for (int q = 0; q <= 100; q++) {
+System.out.println(100-q);
 		}
-
 	}
-
 }
