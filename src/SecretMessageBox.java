ef7eae9fc66e9959e05f2abaf57aaecb14b75091
TheMasterOfIron <league@WTS19.attlocal.net>
2017-08-06 11:29:07 -0700

    ghhjuy
     cfgvhjmn b

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -2,27 +2,20 @@ import javax.swing.JOptionPane;
 
 public class SecretMessageBox {
 	public static void main(String[] args) {
-		String Passcode = "Shadow Wolf";
+		String Name = "";
+		String Passcode = JOptionPane.showInputDialog("What do you want the password to be.");
 		String Message = JOptionPane.showInputDialog("Give me a message to store in the super secure safe.");
+		String Nickname = JOptionPane.showInputDialog("Enter the nickname you want(for extra security).");
 		String Pass = JOptionPane.showInputDialog("Enter passcode to read secret message.");
 		if (Pass.equals(Passcode)) {
-			JOptionPane.showMessageDialog(null, Message + "");
+			Name = JOptionPane.showInputDialog(null, "Correct passcode now enter your username.");
 		}
-		else{JOptionPane.showMessageDialog(null, "I thought point was to break into it not fail horribly while trying.");
+		if (Name.equals(Nickname)) {
+			JOptionPane.showMessageDialog(null, "Message:" + Message);
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"Intruder detected deleting secret message  ...  Secret message deleted.\nYou Better run "
+							+ Nickname + " is coming");
 		}
+	}
 }
-}
-// 0. Make a main method and put steps 1-5 inside it
-
-// 1. Set the passcode in a String variable
-
-// 2. Using a pop-up, ask for a secret message and store it in a variable
-
-// 3. Ask your friend for the passcode and store it in a variable
-
-// 4. If the passcode matches, show the secret message
-
-// 5. If the passcode does not match, pop-up "INTRUDER!!"
-
-// [optional] 6. Have your friend also enter a username, and make sure it is
-// correct before releasing the secret message.
